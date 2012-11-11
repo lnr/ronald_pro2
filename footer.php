b87d35f0724265ea6c29eecea9b15236869dfc6c
lnr <kushchiro@gmail.com>
2012-11-11 22:34:32 +0200

    v1.0

--- footer.php
+++ footer.php
@@ -9,11 +9,11 @@ $title = get_the_title();
 ?>
 				<div class="menu">
 					<ul>
-						<li><a class=<?php echo ($title == 'history') ? '"active"' : '"" href="/history"';?>>History</a></li>
-						<li><a class=<?php echo ($title == 'about') ? '"active"' : '"" href="/about"';?>>About</a></li>
-						<li><a class=<?php echo ($title == 'contact') ? '"active"' : '"" href="/contact"';?>>Contact</a></li>
-						<li><a class=<?php echo ($title == 'projects') ? '"active"' : '"" href="/projects"';?>>Projects</a></li>
-						<li><a class=<?php echo ($title == 'news') ? '"active"' : '"" href="/news"';?>>News</a></li>
+						<li class="menu-history"><a class=<?php echo ($title == 'history') ? '"active"' : '"" href="/history"';?>>History</a></li>
+						<li class="menu-about"><a class=<?php echo ($title == 'about') ? '"active"' : '"" href="/about"';?>>About</a></li>
+						<li class="menu-contact"><a class=<?php echo ($title == 'contact') ? '"active"' : '"" href="/contact"';?>>Contact</a></li>
+						<li class="menu-projects"><a class=<?php echo ($title == 'projects') ? '"active"' : '"" href="/projects"';?>>Projects</a></li>
+						<li class="menu-news"><a class=<?php echo ($title == 'news') ? '"active"' : '"" href="/news"';?>>News</a></li>
 					</ul>
 					<div style="clear:both;"></div>
 				</div> 
